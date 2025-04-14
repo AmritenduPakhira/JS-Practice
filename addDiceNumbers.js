8b62a80b3acbfa0c7210e8abb2c3d21a83d8fe07
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-14 20:23:50 +0530

    Add solution for adding two random dice numbers

---
+++ addDiceNumbers.js
@@ -0,0 +1,7 @@
+// Problem 3: Add two random dice numbers
+const dice1 = Math.floor(Math.random() * 6) + 1;
+const dice2 = Math.floor(Math.random() * 6) + 1;
+const sum = dice1 + dice2;
+console.log("First Dice:", dice1);
+console.log("Second Dice:", dice2);
+console.log("Sum of Dice:", sum); 
