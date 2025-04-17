19ecaad67c243713051672a74a9cf0863430b206
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-17 10:41:07 +0530

    [Amritendu Pakhira] add pizza-closure-example file update

--- Js basic/pizza-closure-example.js
+++ Js basic/pizza-closure-example.js
@@ -1,17 +1,27 @@
 function CreatePizza(flavor){
-    return function deliverepizza(callback){
+ function deliverepizza(){
         console.log("Creating pizza with flavor: " + flavor);
-        callback(flavor);
+        customerfeedback(flavor);
+        customerrarting(flavor);
     }
+
+    return deliverepizza
 }
 
 function customerfeedback(pizza){
     console.log(`Customer feedback for pizza: " ${pizza}`);
 }
 
+function customerrarting(pizza){
+    console.log(`Customer rating for pizza: " ${pizza}`);
+}
+
 const myorder = CreatePizza("Margherita"); // closure
 
-myorder(customerfeedback);
+const myorder2 = CreatePizza("Pepperoni"); // closure
+
+myorder(); 
+
 
 
 
