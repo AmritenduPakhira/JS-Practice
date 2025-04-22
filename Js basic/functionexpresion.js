4f71ac9c6335820eeb6c01481c37f73d722ed203
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-22 22:28:20 +0530

    [Amritendu] add the function that is imediadate revoked function expression

--- Js basic/functionexpresion.js
+++ Js basic/functionexpresion.js
@@ -4,5 +4,15 @@ const myfunction = function (a, b) {
 
     console.log(myfunction(2, 3));  //fucntion expression
 
+    function greet(name){
+        console.log("Hello " + name);
+    }
 
-    
+    greet("John"); 
+
+    (function(){
+console.log("IIFE: This is an immediately invoked function expression.");
+    })
+    ()
+
+
