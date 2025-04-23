09b28211f72a6459e90299cc681679b80210c8ef
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-23 14:24:57 +0530

    [Amritendu] add array.js file and unshift method of the array

---
+++ Js basic/arraymethod/array.js
@@ -0,0 +1,32 @@
+
+// use push mmethod of array
+const cricketer = [{
+    name: "Sachin Tendulkar", age: 23}, {name: "Virat Kohli", age: 35}, {name: "MS Dhoni", age: 40}];
+
+    cricketer.push({name: "Rohit Sharma", age: 35});
+    cricketer.push({name: "Ravindra Jadeja", age: 34});
+
+    console.log(cricketer);
+
+
+
+    // use pop method of array
+    const fotballer = [{name: "Ronaldo", age: 38}, {name: "Messi", age: 36}, {name: "Neymar", age: 31}];
+    console.log(fotballer);
+    fotballer.pop({name: "Ronaldo", age: 38});
+
+    console.log(fotballer);
+
+    // use shift method of array
+
+    const basketballer = [{name: "Kobe Bryant", age: 41}, {name: "Michael Jordan", age: 60}, {name: "LeBron James", age: 38}];
+    console.log(basketballer);
+    basketballer.shift({name: "Kobe Bryant", age: 41});
+    console.log(basketballer);  
+
+    //use unshift method of array
+
+    const hockeyer = [{name: "Wayne Gretzky", age: 62}, {name: "Sidney Crosby", age: 35}, {name: "Alex Ovechkin", age: 37}];
+    console.log(hockeyer);
+    hockeyer.unshift({name: "Wayne Gretzky", age: 62});
+    console.log(hockeyer);
