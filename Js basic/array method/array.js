23fdc1210705265ebff5659bca392d14051420ee
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-23 14:11:12 +0530

    [Amritendu] add array.js file and and push method of array

---
+++ Js basic/array method/array.js
@@ -0,0 +1,9 @@
+
+// use push mmethod of array
+const cricketer = [{
+    name: "Sachin Tendulkar", age: 23}, {name: "Virat Kohli", age: 35}, {name: "MS Dhoni", age: 40}];
+
+    cricketer.push({name: "Rohit Sharma", age: 35});
+    cricketer.push({name: "Ravindra Jadeja", age: 34});
+
+    console.log(cricketer);
