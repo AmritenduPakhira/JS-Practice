44161df78cfbbc8918163ffdfbf3acae166743ac
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-28 10:25:58 +0530

    [Amritendu] fix: correct property name typo in person constructor

---
+++ Js basic/object/objectcocnstructor.js
@@ -0,0 +1,11 @@
+function person(firstname, lastname){
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
+
+let person1 = new person("Amritendu", "Pakhira");
+let person2 = new person("Raj", "Kumar");
+
+console.log(person1.firstname);
+console.log(person2);
+
--- Js basic/object/objectliteral.js
+++ Js basic/object/objectliteral.js
@@ -4,6 +4,7 @@ let person = {
     fullname() {
         return `The name of the person is ${person.firstname} ${person.lastname}`;
     },
+
     adress : {
         street: "123 Main St",
         city: "Anytown",
