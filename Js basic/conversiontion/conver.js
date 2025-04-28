a40d76d7781d9fc9cd4fc4324215e6ca9afd4303
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-28 11:35:43 +0530

    docs: add JavaScript type conversion examples

---
+++ Js basic/conversiontion/conver.js
@@ -0,0 +1,54 @@
+let a = 55;
+console.log(a + "55");
+
+//number to string
+let c = 60;
+console.log( true + c);
+
+//implicit conversion
+const res = (5 == "5");
+console.log(res);
+
+//boolean conversion
+
+let res1 = Boolean("3 ");
+ let res2 = Boolean("hello");
+
+ console.log(res1);
+ console.log(res2);
+
+
+ //explicit conversion
+
+ //number to string
+
+ let n =123;
+ let s1 = String(n);
+ let n1 = n.toString();
+ console.log(s1);
+ console.log(n1);
+
+//string to number
+
+let  m = "123";
+let se = Number(m);
+
+let m2 = "12.34";
+let sw = parseFloat(m2);
+console.log(se);
+console.log(sw);
+
+//boolean 
+
+let str = "hello";
+let b1 = Boolean(str);
+let empty = "";
+let b2 = Boolean(empty);
+console.log(b1);
+console.log(b2);
+
+
+
+
+
+
