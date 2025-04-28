ac920c3e60f37e04e000adf7f139488080fac00f
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-28 13:06:11 +0530

    docs: demonstrate JavaScript equality operators

---
+++ Js basic/operator/equa.js
@@ -0,0 +1,22 @@
+console.log(5 == "5");
+console.log(5 === "5");
+console.log("5" === "5");
+
+let a=7;
+let b = 9;
+a = 9;
+let c = 21;
+
+console.log(a == b);
+console.log(c === b ? "yes" : "no");
+
+let m = 19;
+
+if(m >=18){
+    console.log("you are eligible to vote");
+}else{
+    console.log("you are not eligible to vote");
+}
+
+
+
