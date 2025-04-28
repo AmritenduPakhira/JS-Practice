0bd2a41067146f5b34af1930f44452712300a003
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-28 10:20:51 +0530

    fix: correct object literal syntax and method definition

---
+++ Js basic/object/objectliteral.js
@@ -0,0 +1,17 @@
+let person = {
+    firstname: "John",
+    lastname: "Doe",
+    fullname() {
+        return `The name of the person is ${person.firstname} ${person.lastname}`;
+    },
+    adress : {
+        street: "123 Main St",
+        city: "Anytown",
+        state: "CA",
+        zip: "12345"
+    }
+}
+
+console.log(person.fullname());
+console.log(person.adress.zip);
+
